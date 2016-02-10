223fee739311d8a6695184b48ac1e29cec32b19d
Justin Tulk <justintulk@gmail.com>
2016-02-10 11:38:18 -1000

    updating press articles, tweaking press article styling, updating homepage jonas picture

--- public/dist/js/scripts.js
+++ public/dist/js/scripts.js
@@ -668,20 +668,32 @@ moviePitchApp.factory('pitchFactory', function ($q, $http) {
 
 moviePitchApp.factory('PressFactory', function ($q) {
 	var articles = [{
+		title: "Meet Hollywood's Mr. Pitch",
+		subtitle: "Robert Kosberg has made a career out of pitching in-your-face ideas for movies. Have you heard his pitch for a horror film about a rampaging dog? Think 'Jaws on paws.'' Why not let him direct your next pitch?",
+		url: "http://www.fastcompany.com/38665/meet-hollywoods-mr-pitch"
+	}, {
+		title: "Talking with Bob Kosberg",
+		subtitle: "Talking with Bob Kosberg -- Here's why everyone's buying what Hollywood's fastest talker has to sell.",
+		url: "http://www.ew.com/article/2006/07/28/talking-bob-kosberg"
+	}, {
+		title: "‘Extra!’ base hit for pair",
+		subtitle: "DreamWorks has preemptively bought the pitch “Extra! Extra!” by scribes Bobby Florsheim and Josh Stolberg (“The Passion of the Ark”) for Robert Kosberg to produce.",
+		url: "https://variety.com/2005/film/reviews/extra-base-hit-for-pair-1117924064/"
+	}, {
 		title: "The Art of Selling Ideas",
-		subtitle: "A few moments with Bob Kosberg, Hollywood idea man and pitch master",
+		subtitle: "A few moments with Bob Kosberg, Hollywood idea man and pitch master.",
 		url: "http://www.flightofideas.net/Articles/Quest%20-%20The%20Art%20of%20Selling%20Ideas%20-%20By%20Bob%20Kodzis%20Jan%20Feb%202008.pdf"
 	}, {
 		title: "Concept is King",
-		subtitle: "In today's tight spec-script market, nothing is more important than your idea",
+		subtitle: "In today's tight spec-script market, nothing is more important than your idea.",
 		url: "http://www.scriptmag.com/features/concept-is-king"
 	}, {
 		title: "The Pitch Guy",
-		subtitle: "Some people carve out a career writing screenplays. Others get rich bringing them to life onscreen. Then there’s Robert Kosberg. His specialty? Ideas",
+		subtitle: "Some people carve out a career writing screenplays. Others get rich bringing them to life onscreen. Then there’s Robert Kosberg. His specialty? Ideas.",
 		url: "http://www.lamag.com/longform/the-pitch-guy/"
 	}, {
 		title: "It's the Pitch, Stupid!",
-		subtitle: "An interview with Robert Kosberg",
+		subtitle: "An interview with Robert Kosberg.",
 		url: "http://www.absolutewrite.com/screenwriting/robert_kosberg.htm"
 	}];
 
@@ -813,6 +825,21 @@ moviePitchApp.directive('contactUsForm', function (emailFactory, $timeout) {
     templateUrl: "dist/components/contact-us-form/contact-us-form.html"
   };
 });
+"use strict";
+
+moviePitchApp.directive('appHeader', function ($state) {
+  return {
+    controller: function controller($scope) {
+      $scope.menuToggleStatus = "menu-closed";
+
+      $scope.toggleMenu = function () {
+        $scope.menuToggleStatus = $scope.menuToggleStatus === "menu-closed" ? "menu-open" : "menu-closed";
+      };
+    },
+    restrict: "A",
+    templateUrl: "dist/components/nav/nav.html"
+  };
+});
 'use strict';
 
 moviePitchApp.directive('labelWrapper', function () {
@@ -837,21 +864,6 @@ moviePitchApp.directive('labelWrapper', function () {
       });
     },
     restrict: "A"
-  };
-});
-"use strict";
-
-moviePitchApp.directive('appHeader', function ($state) {
-  return {
-    controller: function controller($scope) {
-      $scope.menuToggleStatus = "menu-closed";
-
-      $scope.toggleMenu = function () {
-        $scope.menuToggleStatus = $scope.menuToggleStatus === "menu-closed" ? "menu-open" : "menu-closed";
-      };
-    },
-    restrict: "A",
-    templateUrl: "dist/components/nav/nav.html"
   };
 });
 'use strict';
--- public/src/factories/pressFactory.js
+++ public/src/factories/pressFactory.js
@@ -1,23 +1,38 @@
 moviePitchApp.factory('PressFactory', function($q){
 	const articles = [
 		{
+			title: "Meet Hollywood's Mr. Pitch",
+			subtitle: "Robert Kosberg has made a career out of pitching in-your-face ideas for movies. Have you heard his pitch for a horror film about a rampaging dog? Think 'Jaws on paws.'' Why not let him direct your next pitch?",
+			url: "http://www.fastcompany.com/38665/meet-hollywoods-mr-pitch"
+		},
+		{
+			title: "Talking with Bob Kosberg",
+			subtitle: "Talking with Bob Kosberg -- Here's why everyone's buying what Hollywood's fastest talker has to sell.",
+			url: "http://www.ew.com/article/2006/07/28/talking-bob-kosberg"
+		},
+		{
+			title: "‘Extra!’ base hit for pair",
+			subtitle: "DreamWorks has preemptively bought the pitch “Extra! Extra!” by scribes Bobby Florsheim and Josh Stolberg (“The Passion of the Ark”) for Robert Kosberg to produce.",
+			url: "https://variety.com/2005/film/reviews/extra-base-hit-for-pair-1117924064/"
+		},
+		{
 			title: "The Art of Selling Ideas",
-			subtitle: "A few moments with Bob Kosberg, Hollywood idea man and pitch master",
+			subtitle: "A few moments with Bob Kosberg, Hollywood idea man and pitch master.",
 			url: "http://www.flightofideas.net/Articles/Quest%20-%20The%20Art%20of%20Selling%20Ideas%20-%20By%20Bob%20Kodzis%20Jan%20Feb%202008.pdf"
 		},
 		{
 			title: "Concept is King",
-			subtitle: "In today's tight spec-script market, nothing is more important than your idea",
+			subtitle: "In today's tight spec-script market, nothing is more important than your idea.",
 			url: "http://www.scriptmag.com/features/concept-is-king"
 		},
 		{
 			title: "The Pitch Guy",
-			subtitle: "Some people carve out a career writing screenplays. Others get rich bringing them to life onscreen. Then there’s Robert Kosberg. His specialty? Ideas",
+			subtitle: "Some people carve out a career writing screenplays. Others get rich bringing them to life onscreen. Then there’s Robert Kosberg. His specialty? Ideas.",
 			url: "http://www.lamag.com/longform/the-pitch-guy/"
 		},
 		{
 			title: "It's the Pitch, Stupid!",
-			subtitle: "An interview with Robert Kosberg",
+			subtitle: "An interview with Robert Kosberg.",
 			url: "http://www.absolutewrite.com/screenwriting/robert_kosberg.htm"
 		}
 	];
