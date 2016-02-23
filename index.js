571f166573003ad21d2d5b141397e9661e1a9052
Justin Tulk <justintulk@gmail.com>
2016-02-23 11:53:33 -1000

    removing htaccess and using forcedomain instead

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const forceDomain = require('forcedomain');
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(forceDomain({
+	hostname: 'www.moviepitch.com'
+}));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
