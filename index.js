6dc0825cabda8f17c68edc349610ecf98b4332ff
Justin Tulk <justintulk@gmail.com>
2016-02-23 12:09:16 -1000

    undoing forcedomain url rewriting

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
-const forceDomain = require('forcedomain');
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(forceDomain({
-	hostname: 'www.moviepitch.com'
-}));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
